2f9a49617d93e35ffd87e3e46c874e8192a78934
O(n) Phelan <phelanowen@gmail.com>
2016-12-04 19:52:10 +0000

    Splash Screen

---
+++ app/src/main/java/com/example/myfirstapp/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
